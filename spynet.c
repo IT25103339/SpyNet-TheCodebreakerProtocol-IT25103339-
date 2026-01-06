29bd33c0fea06a29adefe7ecb7a8eed8ab176b9e
IT25103339 <it25103339@my.sliit.lk>
2026-01-06 22:50:55 +0530

    spynet

---
+++ spynet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define player1 '@'
+#define player2 '&'
+#define player3 '$'
+#define symbol_wall '#'
+#define symbol_life 'L'
+#define symbol_intel 'I'
+#define symbol_extract 'X'
+#define symbol_empty '.'
+
+int main(){
+	int N; // grid size 5 - 15
+
