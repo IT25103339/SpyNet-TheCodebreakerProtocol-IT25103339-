8aaa146bbe44689c8c71ca95309adf51b31560c8
IT25103339 <it25103339@my.sliit.lk>
2026-01-21 21:57:33 +0530

    spyNet

---
+++ spyNet_TCP/spyNet.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PLYR1 '@'
+#define PLYR2 '&'
+#define PLYR3 '$'
+#define SYM_WALL '#'
+#define SYM_LIFE 'L'
+#define SYM_INTEL 'I'
+#define SYM_EXTRACT 'X'
+#define SYM_EMPTY '.'
+
+void gridSize_select(int *gridsize);
+void playerMode_select(int *playermode, int *complayer, char *complayerPrompt);
+
+typedef struct{
+	int x, y;
+	int lives;
+	int intel;
+	char symbol;
+	int alive;
+	int computerAI;
+} Player;
+
+int main(){
+
+	srand(time(NULL));
+
+	int gridSize, playerMode;// grid size 5 - 15
+	int compPlayer = 0;
+	char compPlayerPrompt;
+
+	printf("\n*****SpyNet – The Codebreaker Protocol*****\n");
+	
+	gridSize_select(&gridSize);
+	
+	playerMode_select(&playerMode, &compPlayer, &compPlayerPrompt);
+}
+
+void gridSize_select(int *gridsize){
+	
+	printf("\nEnter grid size (5-15): "); // get grid size
+        scanf(" %d", gridsize);
+
+        while (*gridsize < 5 || *gridsize > 15){ // ask again if the value is not within parameters
+                printf("Error! Value not acceptable. Enter a value between 5-15: ");
+                scanf(" %d", gridsize);
+        }
+	printf("\n");
+}
+
+void playerMode_select(int *playermode, int *compplayer, char *compplayerPrompt){
+
+	printf("Enter player mode(1-3): "); // get player mode single/multiple
+        scanf(" %d", playermode);
+
+        if (*playermode < 1 || *playermode > 3){ //default to singleplayer if value is out of parameters
+                printf("\nError! Defaulting value to 1!\n");
+                *playermode = 1;
+        }
+
+        if (*playermode == 1){ // display selected player mode
+                printf("\nYou have selected Singleplayer mode\n");
+        }else if(*playermode == 2){
+                printf("\nYou have selected 2 player mode\n");
+        }else {
+                printf("\nYou have selected 3 player mode\n");
+        }
+
+        if (*playermode > 1){ // check for computer opponent permission
+                printf("\nDo you want to enable Computer as an opponent (Y/N): ");
+                scanf(" %c", compplayerPrompt);
+
+                if (*compplayerPrompt == 'Y' || *compplayerPrompt == 'y'){ //check for input and select computer/human opponent
+                        *compplayer = 1;
+                        printf("\nComputer opponent enabled!\n");
+                }else if (*compplayerPrompt == 'N' || *compplayerPrompt == 'n'){
+                        *compplayer = 0;
+                        printf("\nHuman opponent mode selected!\n");
+                }else{
+                        printf("\nError! Invalid input! Defaulting to Human Opponent mode\n");
+                        *compplayer = 0;
+                }
+
+        }
+}
