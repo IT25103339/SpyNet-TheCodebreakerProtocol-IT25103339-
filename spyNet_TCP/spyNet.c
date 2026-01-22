b75d242d78e9f74e612c734735c63095736e0583
IT25103339 <it25103339@my.sliit.lk>
2026-01-22 07:13:00 +0530

    spyNet

--- spyNet_TCP/spyNet.c
+++ spyNet_TCP/spyNet.c
@@ -13,6 +13,8 @@
 
 void gridSize_select(int *gridsize);
 void playerMode_select(int *playermode, int *complayer, char *complayerPrompt);
+void createGrid(char ***Grid, int gridSize1);
+void printGrid(char **grid, int gridSize2);
 
 typedef struct{
 	int x, y;
@@ -21,21 +23,26 @@ typedef struct{
 	char symbol;
 	int alive;
 	int computerAI;
-} Player;
+} player_t;
 
 int main(){
 
 	srand(time(NULL));
 
-	int gridSize, playerMode;// grid size 5 - 15
+	int gridSize, playerMode;
 	int compPlayer = 0;
 	char compPlayerPrompt;
+	char **grid = NULL;
 
 	printf("\n*****SpyNet – The Codebreaker Protocol*****\n");
 	
 	gridSize_select(&gridSize);
 	
 	playerMode_select(&playerMode, &compPlayer, &compPlayerPrompt);
+
+	createGrid(&grid, gridSize);
+	
+	printGrid(grid, gridSize);
 }
 
 void gridSize_select(int *gridsize){
@@ -58,16 +65,16 @@ void playerMode_select(int *playermode, int *compplayer, char *compplayerPrompt)
 	while (*playermode < 1 || *playermode > 3){ // ask again if the value is not within parameters
                 printf("Error! Value not acceptable. Enter a value between 1-3: ");
                 scanf(" %d", playermode);
-
-		if (*playermode == 1){ // display selected player mode
-                	printf("\nYou have selected Singleplayer mode\n");
-        	}else if(*playermode == 2){
-                	printf("\nYou have selected 2 player mode\n");
-        	}else {
-                	printf("\nYou have selected 3 player mode\n");
-        	}
 	}
 
+	if (*playermode == 1){ // display selected player mode
+                printf("\nYou have selected Singleplayer mode\n");
+        }else if(*playermode == 2){
+                printf("\nYou have selected 2 player mode\n");
+        }else{
+                printf("\nYou have selected 3 player mode\n");
+        }
+	
 	if (*playermode > 1){
 		while (1){
 
@@ -89,4 +96,55 @@ void playerMode_select(int *playermode, int *compplayer, char *compplayerPrompt)
 		}
 	}
 
+}
+
+void createGrid(char ***Grid, int gridSize1){
+
+	int row = 0, currentRow = 0, currentCol = 0;
+
+	*Grid = (char **)malloc(gridSize1*sizeof(char *));
+
+	while (row < gridSize1){
+
+        	(*Grid)[row] = (char *)malloc(gridSize1 * sizeof(char));
+        	row = row + 1;
+	}
+	
+	while (currentRow < gridSize1){
+      
+        char *thisRow = (*Grid)[currentRow];
+	currentCol = 0;
+
+        while (currentCol < gridSize1){
+            
+            thisRow[currentCol] = SYM_EMPTY;
+            currentCol = currentCol + 1;
+	}
+        
+        currentRow = currentRow + 1;
+	
+	}
+}
+
+
+void printGrid(char **grid, int gridSize2){
+	
+	int row1 = 0, i = 0;
+	
+	printf("\n");
+    
+	while (row1 < gridSize2){
+		i = 0;
+
+		while(i < gridSize2){
+
+			printf("%c ", grid[row1][i]);
+			i = i + 1;
+		}
+
+		printf("\n");
+		row1 = row1 + 1;
+	}
+
+	printf("\n");
 }
